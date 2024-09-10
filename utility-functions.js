6967e7098aa42e0b95fc23096d01f08c7e55ae22
insertYourUserName <insertYourEmail@here.com>
2024-09-10 17:35:33 +0000

    🕋🐣 Checkpoint ./index.html:69253233/104 ./utility-functions.js:69253233/1250

--- utility-functions.js
+++ utility-functions.js
@@ -1,5 +1,18 @@
+function display(message) {
+  document.write(message)
+}
 function output(message) {
   
   window.outputTag.innerHTML = window.outputTag.innerHTML + message;
   
+}
+function ask(message) {
+  var userText = prompt(message);
+  var userNumber = Number(userText);
+  var isNotNumber = isNaN(userNumber);
+    if (isNotNumber){
+      return userText;
+    } else {
+      return userNumber
+    }
 }
