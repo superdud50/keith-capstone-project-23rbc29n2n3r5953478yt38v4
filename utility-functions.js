07ba15661ffada968e6b911cade5c7f930531d97
insertYourUserName <insertYourEmail@here.com>
2024-09-17 03:05:51 +0000

    💂🏂 Checkpoint ./index.html:69253233/546 ./utility-functions.js:69253233/393

--- utility-functions.js
+++ utility-functions.js
@@ -1,1 +1,16 @@
 
+function createColumn(contentString){
+  const columnString = `<div class="col">${contentString} </div>`;
+  return columnString;
+  
+  }
+function createRow(columnsString){
+  const rowString = `<div class="row">${columnsString} </div>`
+  return rowString
+  
+}
+function createContainer(rowString){
+  const containerString =  `<div class = "row">${rowString}
+    </div>`
+  return containerString;
+}
